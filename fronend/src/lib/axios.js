54861fc546a651d495acd1603a88350a568b05bc
Bhanusaraswal <167606958+Bhanusaraswal@users.noreply.github.com>
2026-06-12 17:46:14 +0530

    Fix axios instance configuration for CLIENT_URL

--- fronend/src/lib/axios.js
+++ fronend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
  
-    baseURL: import.meta.env.MODE === "development" 
+    CLIENT_URL: import.meta.env.MODE === "development" 
         ? "http://localhost:5000/api" 
         : "https://ecommerce-2-4h2z.onrender.com", 
     withCredentials: true, // Required to pass cookies/sessions securely
