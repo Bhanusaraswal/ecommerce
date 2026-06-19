c7d725c8278bab61f6476e4d7969d6c351147675
Bhanusaraswal <167606958+Bhanusaraswal@users.noreply.github.com>
2026-06-19 21:57:53 +0530

    Update axios instance to use baseURL
    Changed CLIENT_URL to baseURL in axios instance configuration.

--- fronend/src/lib/axios.js
+++ fronend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- 
-    CLIENT_URL: import.meta.env.MODE === "development" 
+    // Change CLIENT_URL to baseURL
+    baseURL: import.meta.env.MODE === "development" 
         ? "http://localhost:5000/api" 
         : "https://ecommerce-2-4h2z.onrender.com/api", 
     withCredentials: true, // Required to pass cookies/sessions securely
